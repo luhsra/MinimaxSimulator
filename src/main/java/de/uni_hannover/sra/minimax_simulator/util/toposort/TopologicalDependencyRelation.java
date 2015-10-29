6f8d39bf8b35b7a5aa3c5242f0b51b8bcdd2de3d
Philipp Rohde <rohde@sra.uni-hannover.de>
2015-10-29 15:46:31 +0100

    Documentation: documented package util.toposort

--- src/main/java/de/uni_hannover/sra/minimax_simulator/util/toposort/TopologicalDependencyRelation.java
+++ src/main/java/de/uni_hannover/sra/minimax_simulator/util/toposort/TopologicalDependencyRelation.java
@@ -1,6 +1,24 @@
 package de.uni_hannover.sra.minimax_simulator.util.toposort;
 
-public interface TopologicalDependencyRelation<T>
-{
+/**
+ * A dependency relation between two {@link TopologicalSortable}s.
+ *
+ * @param <T>
+ *          the class of the {@code TopologicalSortable}
+ *
+ * @author Martin L&uuml;ck
+ */
+public interface TopologicalDependencyRelation<T> {
+
+	/**
+	 * Checks if the specified {@link TopologicalSortable} depends on the other specified {@link TopologicalSortable}.
+	 *
+	 * @param element
+	 *          the {@code TopologicalSortable} to check the dependency for
+	 * @param dependency
+	 *          the dependency {@code TopologicalSortable}
+	 * @return
+	 *          {@code true} if {@code element} depends on {@code dependency}, {@code false} otherwise
+	 */
 	public boolean dependsOn(T element, T dependency);
 }
--- src/main/java/de/uni_hannover/sra/minimax_simulator/util/toposort/TopologicalSortable.java
+++ src/main/java/de/uni_hannover/sra/minimax_simulator/util/toposort/TopologicalSortable.java
@@ -2,7 +2,21 @@ package de.uni_hannover.sra.minimax_simulator.util.toposort;
 
 import java.util.Set;
 
-public interface TopologicalSortable<T>
-{
+/**
+ * A {@code TopologicalSortable} is a class that can be sorted via a {@link TopologicalSorter}.
+ *
+ * @param <T>
+ *          the class of the {@code TopologicalSortable}
+ *
+ * @author Martin L&uuml;ck
+ */
+public interface TopologicalSortable<T> {
+
+	/**
+	 * Gets the dependencies of the {@code TopologicalSortable}.
+	 *
+	 * @return
+	 *          a set of the dependencies
+	 */
 	public Set<T> getDependencies();
 }
