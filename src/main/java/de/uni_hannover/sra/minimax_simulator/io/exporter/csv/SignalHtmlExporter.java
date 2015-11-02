abc33ae1ba97a619fb215e9c9d1c960a43e34d2c
Philipp Rohde <rohde@sra.uni-hannover.de>
2015-11-02 14:57:02 +0100

    Refactoring: created abstract parent class for signal exporters

--- src/main/java/de/uni_hannover/sra/minimax_simulator/io/exporter/csv/SignalHtmlExporter.java
+++ src/main/java/de/uni_hannover/sra/minimax_simulator/io/exporter/csv/SignalHtmlExporter.java
@@ -1,6 +1,5 @@
 package de.uni_hannover.sra.minimax_simulator.io.exporter.csv;
 
-import com.google.common.base.Strings;
 import de.uni_hannover.sra.minimax_simulator.io.IOUtils;
 import de.uni_hannover.sra.minimax_simulator.model.signal.*;
 import de.uni_hannover.sra.minimax_simulator.model.signal.jump.Jump;
@@ -10,18 +9,13 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
 /**
  * The {@code SignalHtmlExporter} exports the {@link SignalTable} to an HTML file.
  *
  * @author Martin L&uuml;ck
  */
 // TODO: upgrade to HTML5 standards
-// TODO: create abstract parent class SignalExporter
-public class SignalHtmlExporter {
-
-	private final File file;
+public class SignalHtmlExporter extends AbstractSignalExporter {
 
 	/**
 	 * Creates a new instance of the {@code SignalHtmlExporter} and sets the file.
@@ -30,51 +24,10 @@ public class SignalHtmlExporter {
 	 *          the {@code File} to save to
 	 */
 	public SignalHtmlExporter(File file) {
-		this.file = checkNotNull(file, "Invalid Null argument: file");
+		super(file);
 	}
 
-	/**
-	 * Converts an {@code int} to a {@code String} with the length of
-	 * {@link SignalType#getBitWidth()} belonging to the value.
-	 *
-	 * @param val
-	 *          the {@code int} to convert
-	 * @param signal
-	 *          the {@code SignalType} the value belongs to
-	 * @return
-	 *          a binary {@code String} representation of the value
-	 */
-	private String binaryToString(int val, SignalType signal) {
-		return Strings.padStart(Integer.toBinaryString(val), signal.getBitWidth(), '0');
-	}
-
-	/**
-	 * Converts the value of a {@link SignalValue} to a {@code String} with
-	 * the length of {@link SignalType#getBitWidth()} belonging to the value.
-	 *
-	 * @param value
-	 *          the {@code SignalValue} to convert
-	 * @param signal
-	 *          the {@code SignalType} the value belongs to
-	 * @return
-	 *          a binary {@code String} representation of the value
-	 */
-	private String getShortDescription(SignalValue value, SignalType signal) {
-		if (value.isDontCare())
-			return "-";
-		return binaryToString(value.intValue(), signal);
-	}
-
-	/**
-	 * Exports the {@code SignalTable} to the file set via the constructor.
-	 *
-	 * @param table
-	 *          the {@code SignalTable} to export
-	 * @param config
-	 *          the {@code SignalConfiguration} belonging to the {@code SignalTable}
-	 * @throws IOException
-	 *          thrown if the file was not writable
-	 */
+	@Override
 	public void exportSignalTable(SignalTable table, SignalConfiguration config) throws IOException {
 		// write the table to disk
 		Writer wr = null;
