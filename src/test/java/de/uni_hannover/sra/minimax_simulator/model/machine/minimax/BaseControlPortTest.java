74bc1c74ad9d46153ec50a5a0f8836f88c8168ef
Philipp Rohde <rohde@sra.uni-hannover.de>
2015-12-11 13:39:33 +0100

    Tests: reduced LOC for BaseControlPortTest

--- src/test/java/de/uni_hannover/sra/minimax_simulator/model/machine/minimax/BaseControlPortTest.java
+++ src/test/java/de/uni_hannover/sra/minimax_simulator/model/machine/minimax/BaseControlPortTest.java
@@ -12,57 +12,12 @@ import static org.junit.Assert.assertEquals;
 public class BaseControlPortTest {
 
     /**
-     * Tests {@link BaseControlPort#ALU_SELECT_A}.
+     * Tests all values of {@code BaseControlPort}.
      */
     @Test
-    public void testAluSelectA() {
-        BaseControlPort bcp = BaseControlPort.ALU_SELECT_A;
-        assertEquals("Port of ALU_SELECT_A", "ALU_SELECT_A", bcp.port().getName());
+    public void test() {
+        for (BaseControlPort bcp : BaseControlPort.values()) {
+            assertEquals("Port of " + bcp.name(), bcp.name(), bcp.port().getName());
+        }
     }
-
-    /**
-     * Tests {@link BaseControlPort#ALU_SELECT_B}.
-     */
-    @Test
-    public void testAluSelectB() {
-        BaseControlPort bcp = BaseControlPort.ALU_SELECT_B;
-        assertEquals("Port of ALU_SELECT_B", "ALU_SELECT_B", bcp.port().getName());
-    }
-
-    /**
-     * Tests {@link BaseControlPort#MDR_SEL}.
-     */
-    @Test
-    public void testMdrSel() {
-        BaseControlPort bcp = BaseControlPort.MDR_SEL;
-        assertEquals("Port of MDR_SEL", "MDR_SEL", bcp.port().getName());
-    }
-
-    /**
-     * Tests {@link BaseControlPort#MEM_CS}.
-     */
-    @Test
-    public void testMemCS() {
-        BaseControlPort bcp = BaseControlPort.MEM_CS;
-        assertEquals("Port of MEM_CS", "MEM_CS", bcp.port().getName());
-    }
-
-    /**
-     * Tests {@link BaseControlPort#MEM_RW}.
-     */
-    @Test
-    public void testMemRW() {
-        BaseControlPort bcp = BaseControlPort.MEM_RW;
-        assertEquals("Port of MEM_RW", "MEM_RW", bcp.port().getName());
-    }
-
-    /**
-     * Tests {@link BaseControlPort#ALU_CTRL}.
-     */
-    @Test
-    public void testAluCtrl() {
-        BaseControlPort bcp = BaseControlPort.ALU_CTRL;
-        assertEquals("Port of ALU_CTRL", "ALU_CTRL", bcp.port().getName());
-    }
-
 }
