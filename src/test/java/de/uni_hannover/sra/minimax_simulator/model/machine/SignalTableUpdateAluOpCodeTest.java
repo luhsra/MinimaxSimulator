ba85fb08a765116bab6fcbc307fc5d7fa627585c
Philipp Rohde <rohde@sra.uni-hannover.de>
2015-12-11 16:01:39 +0100

    Tests: added tests for SignalTable

---
+++ src/test/java/de/uni_hannover/sra/minimax_simulator/model/machine/SignalTableUpdateAluOpCodeTest.java
@@ -0,0 +1,21 @@
+package de.uni_hannover.sra.minimax_simulator.model.machine;
+
+import de.uni_hannover.sra.minimax_simulator.model.signal.SignalTable;
+import de.uni_hannover.sra.minimax_simulator.model.configuration.alu.AluOperation;
+import org.junit.Test;
+
+/**
+ * Tests the implementation of updating ALU operation codes for a {@link SignalTable}.
+ *
+ * @see MachineSignalTable
+ * @see AluOperation
+ *
+ * @author Philipp Rohde
+ */
+public class SignalTableUpdateAluOpCodeTest {
+
+    @Test
+    public void test() {
+        // TODO
+    }
+}
---
+++ src/test/java/de/uni_hannover/sra/minimax_simulator/model/machine/SignalTableUpdateAluSelCodeTest.java
@@ -0,0 +1,19 @@
+package de.uni_hannover.sra.minimax_simulator.model.machine;
+
+import de.uni_hannover.sra.minimax_simulator.model.signal.SignalTable;
+import org.junit.Test;
+
+/**
+ * Tests the implementation of updating ALU select codes for a {@link SignalTable}.
+ *
+ * @see MachineSignalTable
+ *
+ * @author Philipp Rohde
+ */
+public class SignalTableUpdateAluSelCodeTest {
+
+    @Test
+    public void test() {
+        // TODO
+    }
+}
