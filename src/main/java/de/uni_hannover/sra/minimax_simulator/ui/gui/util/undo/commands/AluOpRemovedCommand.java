419f2607b7a6f191bfd6b6c5f83f618e82cfb387
Philipp Rohde <rohde@sra.uni-hannover.de>
2016-04-12 17:06:38 +0200

    Devel: full undo possible after deletion of an AluOperation

--- src/main/java/de/uni_hannover/sra/minimax_simulator/ui/gui/util/undo/commands/AluOpRemovedCommand.java
+++ src/main/java/de/uni_hannover/sra/minimax_simulator/ui/gui/util/undo/commands/AluOpRemovedCommand.java
@@ -1,18 +1,26 @@
 package de.uni_hannover.sra.minimax_simulator.ui.gui.util.undo.commands;
 
+import de.uni_hannover.sra.minimax_simulator.Main;
 import de.uni_hannover.sra.minimax_simulator.model.configuration.MachineConfiguration;
 import de.uni_hannover.sra.minimax_simulator.model.configuration.alu.AluOperation;
+import de.uni_hannover.sra.minimax_simulator.model.signal.SignalRow;
+import de.uni_hannover.sra.minimax_simulator.model.signal.SignalTable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This {@link Command} is used for removing ALU operations.
  *
  * @author Philipp Rohde
  */
-// TODO: very naive implementation; does not revert the changes made to the signal table after the deletion
 public class AluOpRemovedCommand extends Command {
 
     private final AluOperation op;
     private final MachineConfiguration config;
+    private final List<AluOperation> aluOps;
+    private final List<SignalRow> signalRows;
+    private final SignalTable signalTable;
 
     /**
      * Creates the {@code AluOpRemovedCommand} instance for the change.
@@ -25,6 +33,17 @@ public class AluOpRemovedCommand extends Command {
     public AluOpRemovedCommand(AluOperation op, MachineConfiguration config) {
         this.op = op;
         this.config = config;
+        this.aluOps = new ArrayList<>();
+        this.signalRows = new ArrayList<>();
+
+        for (AluOperation aluOp : config.getAluOperations()) {
+            aluOps.add(aluOp);
+        }
+
+        this.signalTable = Main.getWorkspace().getProject().getSignalTable();
+        for (SignalRow row : signalTable.getRows()) {
+            signalRows.add(new SignalRow(row));
+        }
     }
 
     @Override
@@ -34,7 +53,19 @@ public class AluOpRemovedCommand extends Command {
 
     @Override
     public void undo() {
-        config.addAluOperation(op);
+        for (int i = config.getAluOperations().size() - 1; i >= 0; i--) {
+            config.removeAluOperation(config.getAluOperation(i));
+        }
+        for (AluOperation aluOp : aluOps) {
+            config.addAluOperation(aluOp);
+        }
+
+        for (int j = signalTable.getRowCount() - 1; j >= 0; j--) {
+            signalTable.removeSignalRow(j);
+        }
+        for (SignalRow row : signalRows) {
+            signalTable.addSignalRow(row);
+        }
     }
 
     @Override
